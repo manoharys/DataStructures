b936cf26499abe01be068fc1454722a6c3b092ee
manohar <manumanohar1527@gmail.com>
2020-06-27 16:50:28 +0530

    updated power program

--- C/Recusion in C/Examples/power.c
+++ C/Recusion in C/Examples/power.c
@@ -4,15 +4,22 @@
    eg : 2^5 = 2 * 2 * 2 * 2 * 2;
         m^n = m * m * m *...for n times
 
-        power(m, n) = (m * m * m ... * n-1 times) * m
+   i)   power(m, n) = (m * m * m ... * n-1 times) * m
         therefore,
         power(m, n) = (m, n-1) * m  where  n > 0
 
+   ii)  We can improve the first method by reducing power to half
+        eg : 2^8 is same as (2 * 2)^4  //for even numbers
+             2^9 is same as 2*(2*2)^4  //for odd numbers multiply with 2
+
+        therefore power(m, n) = power((m * m), n/2)
+                  power(m, n) = power((m * m), n/2) * m);
 */
 
  #include<stdio.h>
 
  int power(int m, int n);
+ int powerUpdated(int m, int n);
 
  int main() {
    int m, n;
@@ -21,6 +28,7 @@
    printf(" N value : ");
    scanf("%d", &n);
    printf("\n %d power %d is %d", m, n, power(m, n));
+   printf("\n %d power %d is %d", m, n, powerUpdated(m, n));
  }
 
   int power(int m, int n) {
@@ -29,5 +37,19 @@
     }
     else {
         return power(m, n-1) * m;
+    }
+  }
+
+  int powerUpdated(int m, int n) {
+    if(n == 0) {
+        return 1;
+    }
+    else {
+        if(n % 2 == 0) {
+            return power(m*m, n/2);
+        }
+        else{
+            return 2 * power(m*m, n/2);
+        }
     }
   }
